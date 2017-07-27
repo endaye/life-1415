e27dd4b183a5d8ade9c4dfd9261dbd4f6b55f9ca
Yuancheng Zhang <old.far.hot@gmail.com>
2017-07-27 02:47:51 -0500

    update

---
+++ js/1415.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#1415-nav-en").load("html/nav_en.html");
+    $("#1415-footer-en").load("html/footer_en.html");
+    $("#1415-intro-text-sm").html("<a>1415 W Fullerton Ave</a> Chicago, IL 60614");
+});
